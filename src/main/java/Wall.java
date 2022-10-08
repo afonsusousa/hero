25b50dac726d8306e3c4361e7cfea46b1441d624
afonsusousa <afonso.rocha@outlook.com>
2022-10-08 17:19:20 +0100

    Element abstraction

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -3,32 +3,16 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Wall {
-    private Position position;
+public class Wall extends Element{
 
     public Wall(int x, int y){
-        position = new Position(x, y);
+        super(x, y);
     }
-
-    public void setPosition(Position position){
-        this.position = position;
-    }
-    public Position getPosition(){
-        return this.position;
-    }
-
-
-    public int getX() {return position.getX();}
-    public void setX(int x) {position.setX(x);}
-    public int getY() {return position.getY();}
-    public void setY(int y) { position.setY(y);}
 
     public void draw(TextGraphics graphics){
-
         graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
         graphics.enableModifiers(SGR.BOLD);
-        graphics.putString(new TerminalPosition(position.getX(),
-                position.getY()), "@");
+        graphics.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()), "@");
     }
 
 }
